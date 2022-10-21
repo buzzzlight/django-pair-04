6c254fc4c53a33f04071bced4a736da0b0b15159
Bonbon315 <n202u123@gmail.com>
2022-10-21 11:30:47 +0900

    reviews/detail

--- reviews/views.py
+++ reviews/views.py
@@ -4,7 +4,7 @@ from .models import Review
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-# @login_required
+@login_required
 def create(request):
     if request.method == "POST":
         review_form = ReviewForm(request.POST, request.FILES)
@@ -12,7 +12,7 @@ def create(request):
             review = review_form.save(commit=False)
             review.user = request.user
             review.save()
-            return redirect("reviews:create")
+            return redirect("reviews:index")
     else:
         review_form = ReviewForm()
     context = {"review_form": review_form}
@@ -23,3 +23,12 @@ def index(request):
     reviews = Review.objects.all()
     context = {"reviews": reviews}
     return render(request, "reviews/index.html", context)
+
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    context = {
+        "review": review,
+    }
+    return render(request, "reviews/detail.html", context)
